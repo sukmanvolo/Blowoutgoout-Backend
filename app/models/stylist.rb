f870db541883e92538901ed3bf34374e41e5c86c
Rafale Lizardo Celiz <lizardoceliz@gmail.com>
2019-02-14 17:22:59 -0300

    Stylist create API
      - Remove lat and long validation in the model.

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -1,7 +1,7 @@
 class Stylist < ApplicationRecord
   belongs_to :user
 
-  validates :first_name, :lat, :long, presence: true
+  validates :first_name, presence: true
 
   # emun welcome_kit: [] ask for values
   enum service_type: [:hair, :makeup, :hair_and_makeup]
