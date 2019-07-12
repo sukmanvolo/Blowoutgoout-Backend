23e690e6e3b364394462558cedfb6c773df720b1
John Henderson <sqlninja@sqldojo.com>
2019-07-12 09:44:58 -0500

    Gets the sign up wizard working, but still needs to handle form uplads

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -21,21 +21,23 @@ class SignupController < ApplicationController
       @user.update_attributes!(params[:user].permit(:phone, :address, :city, :state, :postal_code))
       render_wizard @user
     when :forms
-      @stylist = Stylist.find_or_create_by(params[:stylist].permit(
-                                             :years_of_experience, :license_agreement,
-                                             :has_smartphone, :has_transportation,
-                                             :portfolio_link, :is_eligible_to_work_in_us,
-                                             :previous_contractor_date, :has_conviction,
-                                             :agrees_to_unemployment_understanding,
-                                             :agrees_to_taxation_understanding
+      @user = current_user || User.find(params[:stylist_id])
 
-                                           ))
+      # TODO: fix file uploads:
+      # ,
+      # :image, :cosmetology_license, :liability_insurance,
+      # :eligibility_document
+
+      @stylist = Stylist.find_or_create_by(params[:stylist].except(:image, :cosmetology_license, :liability_insurance,
+                                                                   :eligibility_document).merge(user_id: @user.id).permit(
+                                                                     :years_of_experience, :license_agreement,
+                                                                     :has_smartphone, :has_transportation,
+                                                                     :portfolio_link, :is_eligible_to_work_in_us,
+                                                                     :previous_contractor_date, :has_conviction,
+                                                                     :agrees_to_unemployment_understanding,
+                                                                     :agrees_to_taxation_understanding, :service_type, :user_id
+                                                                   ))
+      render_wizard @user
     end
   end
 end
-
-
-# TODO: fix file uploads:
-# ,
-# :image, :cosmetology_license, :liability_insurance,
-# :eligibility_document
