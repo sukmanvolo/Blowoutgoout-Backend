1ca43d2d8ba7d456a52da6d9377d217e8b54a17d
Rafael Lizardo Celiz <lizardoceliz@gmail.com>
2019-07-25 18:16:39 -0300

    Default Avatar
      - Add default avatar only until we have a S3 bucket.

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -2,10 +2,13 @@ class ClientSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :phone, :image
 
   def image
-    return nil unless object.image_attached?
+    if object.image_attached?
       Rails.application
            .routes
            .url_helpers
            .rails_representation_url(object.image.variant(resize: "100x100").processed)
+    else
+      'https://i.pravatar.cc/150?img=1'
+    end
   end
 end
--- app/serializers/stylist_serializer.rb
+++ app/serializers/stylist_serializer.rb
@@ -2,10 +2,13 @@ class StylistSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :phone, :lat, :long, :image
 
   def image
-    return nil unless object.image_attached?
+    if object.image_attached?
       Rails.application
            .routes
            .url_helpers
            .rails_representation_url(object.image.variant(resize: "100x100").processed)
+    else
+      'https://i.pravatar.cc/150?img=9'
+    end
   end
 end
