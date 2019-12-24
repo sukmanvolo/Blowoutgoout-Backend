beddbe7253e13aa819960d94c9e5de66b19ccb69
Attiq Rao <attiqrao@Attiqs-MacBook-Pro.local>
2019-12-24 19:37:29 +0500

    fixed reviews

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,8 +1,10 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :text, :rate, :booking_id, :status, :client, :stylist
 
-  belongs_to :client, serializer: ClientSerializer
-  
+  def client
+    client.find_by_id(object&.client).as_json
+  end
+
   def stylist
     Stylist.find_by_id(object&.booking&.stylist_id).as_json
   end
