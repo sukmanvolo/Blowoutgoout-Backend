c18b99a23a1073c33226c47ec41a076f48a4e824
Attiq Rao <attiqrao@Attiqs-MacBook-Pro.local>
2019-12-24 15:49:29 +0500

    Added client and stylist in messages

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,6 +1,6 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :booking_id, :client_id, :stylist_id, :text, :status, :sender,
-             :created_at, :updated_at
+  attributes :id, :booking_id, :text, :status, :sender,
+             :created_at, :updated_at, :client, :stylist
 
   def created_at
     object.created_at.strftime('%m-%d-%Y %H:%M:%S')
@@ -8,5 +8,13 @@ class MessageSerializer < ActiveModel::Serializer
 
   def updated_at
     object.updated_at.strftime('%m-%d-%Y %H:%M:%S')
+  end
+
+  def client
+    Client.find_by_id(object&.client_id).as_json
+  end
+
+  def stylist
+    Stylist.find_by_id(object&.stylist_id).as_json
   end
 end
